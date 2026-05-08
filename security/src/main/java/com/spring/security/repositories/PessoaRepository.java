fb110f0cad4ac7ba0cf9b47945d635371eb88623
unknown <helo.saito@outlook.com>
2026-05-07 21:47:22 -0300

    repository criado

---
+++ security/src/main/java/com/spring/security/repositories/PessoaRepository.java
@@ -0,0 +1,7 @@
+package com.spring.security.repositories;
+
+import com.spring.security.models.PessoaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PessoaRepository extends JpaRepository<PessoaModel, Long> {
+}
